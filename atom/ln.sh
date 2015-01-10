cc836fcecaa91b0521e9a4a243f6982270663826
Hebi Li <lihebi.ga@gmail.com>
2015-01-09 22:19:52 -0600

    use hardlink

--- atom/ln.sh
+++ atom/ln.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ln -sf $HOME/.hebi/atom/styles.less $HOME/.atom/styles.less
-ln -sf $HOME/.hebi/atom/keymap.cson $HOME/.atom/keymap.cson
-ln -sf $HOME/.hebi/atom/snippets.cson $HOME/.atom/snippets.cson
+ln -f $HOME/.hebi/atom/styles.less $HOME/.atom/styles.less
+ln -f $HOME/.hebi/atom/keymap.cson $HOME/.atom/keymap.cson
+ln -f $HOME/.hebi/atom/snippets.cson $HOME/.atom/snippets.cson
