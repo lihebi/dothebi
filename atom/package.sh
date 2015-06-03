3d4892e2710ed78f2c1a051a6f79c033ce53a6cc
Hebi Li <lihebi.com@gmail.com>
2015-06-03 16:14:42 -0500

    add some autocomplete plus providers

--- atom/package.sh
+++ atom/package.sh
@@ -10,3 +10,6 @@ apm install project-manager
 apm install resize-panes
 apm install atom-beautify
 apm install linter
+apm install autocomplete-paths
+apm install autocomplete-bibtex
+apm install autocomplete-clang
