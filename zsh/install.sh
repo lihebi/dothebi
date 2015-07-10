1d0958ebc0d0af991029ae215ce6fa4c08a36f5f
Hebi Li <lihebi.com@gmail.com>
2015-07-10 09:21:30 -0500

    better zshrc; add hebi.zsh-theme

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,1 +1,7 @@
+rm -rf $HOME/.zshrc
 ln -sf $HOME/.hebi/zsh/zshrc $HOME/.zshrc
+if [ -z $ZSH ]; then
+  echo "\$ZSH not set. Setting to $HOME/.oh-my-zsh"
+  export ZSH=$HOME/.oh-my-zsh
+fi
+ln -sf $HOME/.hebi/zsh/hebi.zsh-theme $ZSH/custom/
