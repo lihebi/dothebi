6f3ce330724baf6bfff08fe53adde580f997c4cc
Hebi Li <lihebi.com@gmail.com>
2016-08-23 17:47:37 -0500

    restructure

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# linux
+ln -sf $HOME/.hebi/xinitrc $HOME/.xinitrc
+ln -sf $HOME/.hebi/Xresources $HOME/.Xresources
+ln -sf $HOME/.hebi/stumpwmrc $HOME/.stumpwmrc
+ln -sf $HOME/.hebi/Xmodmap $HOME/.Xmodmap
+ln -sf $HOME/.hebi/ratpoisonrc $HOME/.ratpoisonrc
+ln -sf $HOME/.hebi/conkyrc $HOME/.conkyrc
+ln -sf $HOME/.hebi/conkerorrc $HOME/.conkerorrc
+
+# app
+ln -sf $HOME/.hebi/stumpwm.d $HOME/.stumpwm.d
+
+ln -sf $HOME/.hebi/tmux.conf $HOME/.tmux.conf
+
+# bash
+ln -sf $HOME/.hebi/bashrc $HOME/.bashrc
+ln -sf $HOME/.hebi/bash_profile $HOME/.bash_profile
+ln -sf $HOME/.hebi/bash_prompt $HOME/.bash_prompt
--- tmux/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ln -sf $HOME/.hebi/tmux/tmux.conf $HOME/.tmux.conf
