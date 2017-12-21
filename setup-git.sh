8ac300a1ba9a4c4665f4eab66dfc813aa77038f6
Hebi Li <lihebi.com@gmail.com>
2017-12-20 23:24:26 -0600

    git store

--- setup-git.sh
+++ setup-git.sh
@@ -2,5 +2,6 @@
 
 git config --global user.name "Hebi Li";
 git config --global user.email "lihebi.com@gmail.com";
-git config --global credential.helper cache;
+# default to ~/.my-credentials
+git config --global credential.helper store;
 git config --global push.default simple;
