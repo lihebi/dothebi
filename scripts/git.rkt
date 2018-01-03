7594c92173d784da6d3f4399d70a4d72fdeca829
Hebi Li <lihebi.com@gmail.com>
2018-01-03 02:04:01 -0600

    better output

--- scripts/git.rkt
+++ scripts/git.rkt
@@ -2,28 +2,43 @@
 
 ;; git pull and push all my work
 
-
-;; TODO return (exit-code, output)
 (define (git-op repo op)
-  (let ([cmd (case op
-               [(pull) "git pull"]
-               [(status) "git status"])])
+  (let ([arg (case op
+               [(pull) "pull"]
+               [(status) "status"]
+               [else (raise-syntax-error #f "unsupported git command" op)])])
     (parameterize ([current-directory (expand-user-path repo)])
-      ;; (displayln (string-append "Processing " repo " ..."))
-      (displayln
-       (format "End: Process ~a, with output~n~a~n"
-               repo
-               (with-output-to-string
-                 (lambda ()
-                   (case (system cmd #:set-pwd? #t)
-                     [(#t) (displayln (string-append "Succeeded " repo))]
-                     [(#f) (displayln (string-append "Failed " repo))]))))))))
+      (let-values ([(sp stdout stdin _stderr)
+                    (subprocess #f #f 'stdout (find-executable-path "git") arg)])
+        ;; block
+        (subprocess-wait sp)
+        (let ([exit (subprocess-status sp)]
+              [output (port->string stdout)])
+          (close-output-port stdin)
+          (close-input-port stdout)
+          (values exit output))))))
+
+;; (subprocess (current-output-port) #f (current-error-port) "/usr/bin/pwd")
+;; (find-executable-path "echo")
 
 (define (git-pull repo)
-  (git-op repo 'pull))
-(define (git-status repo)
-  (git-op repo 'status))
+  (let-values ([(exit out) (git-op repo 'pull)])
+    (cond
+      [(not (zero? exit)) (display (format "fatal: ~a failed" repo))]
+      [(string-contains? out "Already up to date") (displayln (format "~a: Up to date" repo))]
+      [else (displayln (~a repo ": Pulled"))
+            (displayln out)])))
 
+(define (git-status repo)
+  (let-values ([(exit out) (git-op repo 'status)])
+    (cond
+      [(not (zero? exit)) (display (format "fatal: ~a failed" repo))]
+      [(string-contains? out "working tree clean") (displayln (format "~a: clean" repo))]
+      [else (displayln (~a repo ": dirty"))
+            (displayln out)])))
+
+;; (git-pull "~/github/wiki")
+;; (git-status "~/github/wiki")
 
 (define (join-threads threads)
   (displayln (format "Syncing on ~a threads .." (length threads)))
@@ -43,6 +58,7 @@
                      repos)))
 
 (module+ test
+  ;; TODO download missing repo
   (define all-repos '("~/github/wiki"
                       "~/github/bibliography"
                       "~/github/books"
