ab4f4cf8717a81011eed952ce334b0638fb3a206
Hebi Li <lihebi.com@gmail.com>
2018-01-03 00:20:25 -0600

    add git-op

--- scripts/git.rkt
+++ scripts/git.rkt
@@ -2,32 +2,63 @@
 
 ;; git pull and push all my work
 
+(define (git-op repo op)
+  (let ([cmd (case op
+               [(pull) "git pull"]
+               [(status) "git status"])])
+    (parameterize ([current-directory (expand-user-path repo)])
+      ;; (displayln (string-append "Processing " repo " ..."))
+      (displayln
+       (format "End: Process ~a, with output~n~a~n"
+               repo
+               (with-output-to-string
+                 (lambda ()
+                   (case (system cmd #:set-pwd? #t)
+                     [(#t) (displayln (string-append "Succeeded " repo))]
+                     [(#f) (displayln (string-append "Failed " repo))]))))))))
+
 (define (git-pull repo)
-  (parameterize ([current-directory (expand-user-path repo)])
-    (displayln (string-append "Pulling " repo " ..."))
-    (case (system "git pull"
-                  #:set-pwd? #t)
-      [(#t) (displayln (string-append "Succeeded " repo))]
-      [(#f) (displayln (string-append "Failed " repo))])))
+  (git-op repo 'pull))
+(define (git-status repo)
+  (git-op repo 'status))
 
-(define all-repos '("~/github/wiki"
-                    "~/github/bibliography"
-                    "~/github/books"
-                    "~/github/hebicc"
-                    "~/github/helium"
-                    "~/github/note"
-                    "~/github/papers"
-                    "~/github/scratch/"
-                    "~/.hebi"
-                    "~/.stumpwm.d"
-                    "~/.emacs.d"
-                    ))
 
+(define (join-threads threads)
+  (displayln (format "Syncing on ~a threads .." (length threads)))
+  (when (not (empty? threads))
+    (let ([ready-thread (apply sync threads)])
+      (join-threads (remove ready-thread threads)))))
+
+(define (git-pull-repos repos)
+  (join-threads (map (lambda (repo)
+                       (thread (lambda ()
+                                 (git-pull repo))))
+                     repos)))
+(define (git-status-repos repos)
+  (join-threads (map (lambda (repo)
+                       (thread (lambda ()
+                                 (git-status repo))))
+                     repos)))
+
+(module+ test
+  (define all-repos '("~/github/wiki"
+                      "~/github/bibliography"
+                      "~/github/books"
+                      "~/github/hebicc"
+                      "~/github/helium"
+                      "~/github/note"
+                      "~/github/papers"
+                      "~/github/scratch/"
+                      "~/.hebi"
+                      "~/.stumpwm.d"
+                      "~/.emacs.d"
+                      ))
+  (git-pull-repos all-repos)
+  (git-status-repos all-repos))
+
+#;
 (define (git-pull-all)
-  (let loop ([threads (map (lambda (repo)
-                             (thread (lambda ()
-                                       (git-pull repo))))
-                           all-repos)])
+  (let loop ([threads ])
     (displayln "Syncing ..")
     (when (not (empty? threads))
       (let ([ready-thread (apply sync threads)])
