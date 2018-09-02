70a367a19b33e288cefab559bdec7eaf0a9ec692
Hebi Li <lihebi.com@gmail.com>
2018-09-01 22:42:25 -0500

    remove some packages for smooth GuixSD config

--- setup-quicklisp.sh
+++ setup-quicklisp.sh
@@ -3,12 +3,18 @@
 # this is still interactive
 # the path is only for archlinux
 # sudo sbcl --quit --load /usr/lib/quicklisp/setup\
+    
+# in case of GuixSD, the sbcl-stumpwm package already has the
+# following three packages, so if install them again, common lisp will
+# complain
+    
+     # --eval '(ql:quickload "clx")'\
+     # --eval '(ql:quickload "cl-ppcre")'\
+     # --eval '(ql:quickload "alexandria")'\
+     # --eval '(ql:quickload "stumpwm")'\
+     
 sbcl --quit\
-     --eval '(ql:quickload "clx")'\
-     --eval '(ql:quickload "cl-ppcre")'\
-     --eval '(ql:quickload "alexandria")'\
      --eval '(ql:quickload "clx-truetype")'\
-     --eval '(ql:quickload "stumpwm")'\
      --eval '(ql:quickload "zpng")'\
      --eval '(ql:quickload "swank")'\
      -non-interactive
